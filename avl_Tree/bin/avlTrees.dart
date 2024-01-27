bf3a81a89d9af51ee2d4664c288cb500ec30898c
shuige <wjuid@163.com>
2024-01-27 15:49:34 +0800

    1/27/2024 3:49

--- avl_Tree/bin/avlTrees.dart
+++ avl_Tree/bin/avlTrees.dart
@@ -1,1 +1,28 @@
-void main() {}
+import 'package:avlTrees/avl_tree.dart';
+
+void main() {
+  final tree = inserting();
+  print(tree);
+  tree.remove(3);
+  print(tree);
+  //removing();
+}
+
+AvlTree<num> inserting() {
+  final tree = AvlTree<num>();
+  for (var i = 0; i < 15; i++) {
+    tree.insert(i);
+  }
+  return tree;
+}
+
+void removing() {
+  final tree = AvlTree<num>();
+  tree.insert(15);
+  tree.insert(10);
+  tree.insert(16);
+  tree.insert(18);
+  print(tree);
+  tree.remove(10);
+  print(tree);
+}
